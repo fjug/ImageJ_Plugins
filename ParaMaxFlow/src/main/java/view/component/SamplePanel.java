99c2ba425193b9064726c6b6d7d6c79358b7ce28
HongKee Moon <hkmoon@me.com>
2014-04-04 15:53:25 +0200

    SamplePanel is modified to demonstrate IDDEA component for noImg constructor.

--- ParaMaxFlow/src/main/java/view/component/SamplePanel.java
+++ ParaMaxFlow/src/main/java/view/component/SamplePanel.java
@@ -4,11 +4,17 @@ import controller.tool.SpimTool;
 import ij.ImagePlus;
 import ij.io.Opener;
 import model.figure.DrawFigureFactory;
+import net.imglib2.RandomAccessible;
+import net.imglib2.RandomAccessibleInterval;
+import net.imglib2.converter.Converters;
+import net.imglib2.converter.RealDoubleConverter;
 import net.imglib2.img.ImagePlusAdapter;
 import net.imglib2.img.Img;
 import net.imglib2.realtransform.AffineTransform2D;
 import net.imglib2.type.numeric.integer.LongType;
+import net.imglib2.type.numeric.real.DoubleType;
 import net.imglib2.type.numeric.real.FloatType;
+import net.imglib2.view.Views;
 import org.jhotdraw.draw.*;
 import org.jhotdraw.draw.action.ButtonFactory;
 import org.jhotdraw.draw.io.DOMStorableInputOutputFormat;
@@ -22,60 +28,78 @@ import view.viewer.InteractiveRealViewer2D;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 import java.util.*;
 
 /**
- * Created with IntelliJ IDEA.
- *
+ * SamplePanel for demonstrating IDDEA component.
+ * 
  * @author HongKee Moon
  * @version 0.1beta
  * @since 9/4/13
  */
-public class SamplePanel extends JPanel {
+public class SamplePanel extends JPanel implements ActionListener {
 
-    IddeaComponent annotator;
+	IddeaComponent annotator;
 
-    public SamplePanel()
-    {
-        super(new BorderLayout());
+	private JButton bLoad;
 
-        File file = new File( "/Users/moon/Projects/ScientificPlatform/ImgLib2/imglib/examples/DrosophilaWing.tif" );
+	public SamplePanel() {
+		super( new BorderLayout() );
 
-        // open a file with ImageJ
-        final ImagePlus imp = new Opener().openImage( file.getAbsolutePath() );
+		annotator = new IddeaComponent();
+		annotator.setToolBarLocation( BorderLayout.WEST );
+		annotator.setToolBarVisible( true );
 
-        // wrap it into an ImgLib image (no copying)
-        final Img<FloatType> image = ImagePlusAdapter.wrap(imp);
+		annotator.setPreferredSize( new Dimension( 30, 20 ) );
 
-        annotator = new IddeaComponent();
-        annotator.setToolBarLocation(BorderLayout.WEST);
-        annotator.setToolBarVisible(true);
+		bLoad = new JButton( "Load" );
+		bLoad.addActionListener( this );
 
-        annotator.setPreferredSize(new Dimension(30, 20));
+		add( annotator, BorderLayout.CENTER );
+		add( bLoad, BorderLayout.NORTH );
 
-        add(annotator, BorderLayout.CENTER);
-
-        //Working
+		//Working
 //        annotator.loadAnnotations("/Users/moon/Desktop/1.xml");
 //        annotator.saveAnnotations("/Users/moon/Desktop/4.xml");
 //        annotator.setImg(image);
 
-        // Doing
+		// Doing
 //        annotator.enableTool(HongKeeViewer.HAND_TOOL);
 //        annotator.enableTool(HongKeeViewer.ZOOM_TOOL);
 //        annotator.enableTool(HongKeeViewer.BEZIER_TOOL);
 //
-    }
+	}
 
-    public static void main( final String[] args ) {
+	public static void main( final String[] args ) {
 
-        SamplePanel panel = new SamplePanel();
+		SamplePanel panel = new SamplePanel();
 
-        final JFrame f = new JFrame();
-        f.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
-        f.getContentPane().add(panel);
-        f.setSize(800, 600);
-        f.setVisible( true );
-    }
+		final JFrame f = new JFrame();
+		f.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+		f.getContentPane().add( panel );
+		f.setSize( 800, 600 );
+		f.setVisible( true );
+	}
+
+	@Override
+	public void actionPerformed( ActionEvent actionEvent ) {
+		if ( actionEvent.getSource().equals( bLoad ) ) {
+			File file = new File( "/Users/moon/Documents/clown.tif" );
+
+			// open a file with ImageJ
+			final ImagePlus imp = new Opener().openImage( file.getAbsolutePath() );
+
+			// wrap it into an ImgLib image (no copying)
+			final Img< DoubleType > image = ImagePlusAdapter.wrap( imp );
+
+			RandomAccessibleInterval imgOrig = Converters.convert( Views.interval( image, image ), new RealDoubleConverter(), new DoubleType() );
+
+			//Views.interval(imgOrig, imgOrig)
+
+			annotator.setDoubleTypeScreenImage( Views.interval( imgOrig, imgOrig ) );
+		}
+	}
 }
