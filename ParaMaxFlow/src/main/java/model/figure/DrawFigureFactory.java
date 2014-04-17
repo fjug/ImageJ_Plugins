3920065e838943339d7d3ef6abf7a2382d2e6551
HongKee Moon <hkmoon@me.com>
2014-04-17 11:40:04 +0200

    1. Transparent Color information is stored. 2. The other shape are stored (LineFigure, EllipseFigure, TriangleFigure, RoundRectangleFigure, TextFigure, RectangleFigure, TextAreaFigure, ImageFigure)

--- ParaMaxFlow/src/main/java/model/figure/DrawFigureFactory.java
+++ ParaMaxFlow/src/main/java/model/figure/DrawFigureFactory.java
@@ -1,15 +1,23 @@
 package model.figure;
 
+import model.xml.DefaultDOMFactory;
 import org.jhotdraw.draw.AttributeKeys;
 import org.jhotdraw.draw.BezierFigure;
 import org.jhotdraw.draw.DefaultDrawing;
 import org.jhotdraw.draw.GroupFigure;
 import org.jhotdraw.draw.QuadTreeDrawing;
-import org.jhotdraw.draw.connector.ChopBezierConnector;
-import org.jhotdraw.xml.DefaultDOMFactory;
+import org.jhotdraw.draw.LineFigure;
+import org.jhotdraw.draw.EllipseFigure;
+import org.jhotdraw.draw.TriangleFigure;
+import org.jhotdraw.draw.RoundRectangleFigure;
+import org.jhotdraw.draw.TextFigure;
+import org.jhotdraw.draw.RectangleFigure;
+import org.jhotdraw.draw.TextAreaFigure;
+import org.jhotdraw.draw.ImageFigure;
 
 /**
  * DrawFingureFactory provides how to store DOM model for the drawing objects.
+ * Color store information is added.
  *
  * @version 0.1beta
  * @since 8/12/13 5:03 PM
@@ -23,9 +31,15 @@ public class DrawFigureFactory extends DefaultDOMFactory {
         { BezierFigure.class, "b" },
         { BezierFigure.class, "bezier" },
         { GroupFigure.class, "g" },
-        { java.awt.Color.class, "color" },
-        
-        { ChopBezierConnector.class, "bezierConnector" },        
+        { java.awt.Color.class, "color" },	// for storing color.
+        { LineFigure.class, "l" },
+        { EllipseFigure.class, "e" },
+        { TriangleFigure.class, "triangle" },
+        { RectangleFigure.class, "r" },
+        { RoundRectangleFigure.class, "rr" },
+        { TextFigure.class, "t" },
+        { TextAreaFigure.class, "ta" },
+        { ImageFigure.class, "image" },
     };
     
     private final static Object[][] enumTagArray = {
@@ -44,5 +58,4 @@ public class DrawFigureFactory extends DefaultDOMFactory {
             addEnumClass((String) o[1], (Class) o[0]);
         }
     }
-
 }
